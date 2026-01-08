a7c47699ca352b05c61223004ee4fd1a859a2ae0
cgmobility <caiogcg.mobilidade@gmail.com>
2026-01-08 14:44:17 -0300

    upload surveys

--- copy files.R
+++ copy files.R
@@ -4,16 +4,41 @@ library(LDA)
 
 wd()
 
+config_gargle_drive()
+
 dirs <- list.dirs(path = '..')
 
 dirs <- dirs[str_detect(dirs,'Pesquisas-Antes-e-Depois',T)]
 
-dirs
+lapply(dirs, function(x){
+  files <- x %>% list.files(recursive = F,
+                            full.names = T,
+                            pattern = '.gsheet')
+  
+  if(length(files)>0){
+    id <- normalizePath(files) %>% 
+      paste0('type "',.,':user.drive.id"') %>% 
+      shell(intern = T)
+    
+    if(!dir.exists(str_remove(x,'\\.\\.\\/'))){
+      dir.create(str_remove(x,'\\.\\.\\/'),recursive = T)
+    }
+    
+    f <- str_remove(files,'\\.\\.\\/') %>% 
+      str_replace('.gsheet','.xlsx')
+    if(!file.exists(f)){
+      drive_download(
+        file = as_id(id),
+        path = f
+      )
+    }
+    
+  }
+  
+})
 
-dirs[3] %>% list.files(recursive = F,
-                       full.names = T,
-                       pattern = '.gsheet') %>% 
-  normalizePath() %>% 
-  paste0('type "',.,':user.drive.id"') %>% 
-  shell()
+
+
+
+
 
